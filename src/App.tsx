2d67d144aafe27a7e308531fe14b5190416ebbe0
Ermiyas <ermiyas.dagnachew@gmail.com>
2023-05-31 13:31:56 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hola yo soy dora</p>
+    </>
+  );
+}
+
+export default App;
